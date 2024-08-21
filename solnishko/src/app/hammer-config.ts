c330efef1fa3b27adb5aa0b81d2a5102409d9c61
vladopot <vladsopot@mail.ru>
2024-08-21 21:52:01 +0200

    add web page

--- solnishko/src/app/hammer-config.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { HammerGestureConfig } from '@angular/platform-browser';
-
-@Injectable()
-export class MyHammerConfig extends HammerGestureConfig {
-  override overrides = {
-    swipe: { direction: Hammer.DIRECTION_ALL },
-  };
-}
